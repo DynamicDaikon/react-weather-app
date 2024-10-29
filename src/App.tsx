34d2e2c08211d6891b95cf1453d6b4c09c75c573
DynamicDaikon <hietasoba@gmail.com>
2024-10-29 16:46:51 +0900

    migrate component props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Title from './components/Title';
 import Form from './components/Form';
+import Results from './components/Result';
+import { useState } from 'react';
 
 const App = () => {
+  const [city, setCity] = useState<string>('');
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch(
+      'https://api.weatherapi.com/v1/current.json?key=578a4942a2b7489c96670138242810&q=London&aqi=no',
+    )
+      .then((res) => console.log(res.json()))
+      .then((data) => console.log(data));
+  };
   return (
     <div>
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather} />
+      <Results />
     </div>
   );
 };
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,26 +1,18 @@
-import { useState } from 'react';
+type FormProps = {
+  setCity: React.Dispatch<React.SetStateAction<string>>;
+  getWeather: (e: any) => void;
+};
 
-const Form = () => {
-  const [city, setCity] = useState<string>('');
-
-  const getWeather = (e: any) => {
-    e.preventDefault();
-    fetch(
-      'https://api.weatherapi.com/v1/current.json?key=578a4942a2b7489c96670138242810&q=London&aqi=no',
-    )
-      .then((res) => console.log(res.json()))
-      .then((data) => console.log(data));
-  };
+const Form = (props: FormProps) => {
   return (
     <form>
       <input
         type="text"
         name="city"
         placeholder="都市名"
-        onChange={(e) => setCity(e.target.value)}
+        onChange={(e) => props.setCity(e.target.value)}
       />
-      {city}
-      <button type="submit" onClick={getWeather}>
+      <button type="submit" onClick={props.getWeather}>
         Get Weather
       </button>
     </form>
